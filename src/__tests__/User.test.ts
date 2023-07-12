be5020b8e2f00785937e0d9a4004f75bd653bf27
Matheus Silverio <matheusphillipo@gmail.com>
2023-07-11 23:17:47 -0300

    tests: case tests to delete user method

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,8 +1,9 @@
 import request from "supertest";
-import { getConnection } from "typeorm";
+import { getConnection, getCustomRepository } from "typeorm";
 import { app } from "../app";
 
 import createConnection from "../database";
+import { UsersRepository } from "../repositories/UsersRespository";
 
 describe("Users", () => {
   beforeAll(async () => {
@@ -15,7 +16,6 @@ describe("Users", () => {
     await connection.dropDatabase();
     await connection.close();
   });
-
   it("Should be able to create a new user", async () => {
     const response = await request(app).post("/users").send({
       email: "user@example.com",
@@ -24,7 +24,6 @@ describe("Users", () => {
 
     expect(response.status).toBe(201);
   });
-
   it("Should not be able to create a user with exists email", async () => {
     const response = await request(app).post("/users").send({
       email: "user@example.com",
@@ -32,5 +31,43 @@ describe("Users", () => {
     });
 
     expect(response.status).toBe(400);
+  });
+
+  it("Should be able to delete a user", async () => {
+    const user = await request(app).post("/users").send({
+      email: "fulano@example.com",
+      name: "Fulano",
+    });
+    const response = await request(app).delete(`/users/${user.body.id}`);
+
+    expect(response.status).toBe(204);
+  });
+
+  it("Should not be able to delete a user that don't exists", async () => {
+    const user = {
+      id: "123",
+      name: "Fulano",
+      email: "fulano@example.com",
+    };
+
+    const response = await request(app).delete(`/users/${user.id}`);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("Should remove the user from the database", async () => {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await request(app).post("/users").send({
+      email: "fulano@example.com",
+      name: "Fulano",
+    });
+
+    const response = await request(app).delete(`/users/${user.body.id}`);
+
+    expect(response.status).toBe(204);
+
+    const deletedUser = await usersRepository.findOne(user.body.id);
+
+    expect(deletedUser).toBeUndefined();
   });
 });
